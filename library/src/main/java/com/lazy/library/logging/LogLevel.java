d9bb12f1403386b6ee9a1badf56a87d686ff5344
rentianlong <1772885836@qq.com>
2017-04-15 21:55:15 +0800

    [+] add New Function LogTransaction

---
+++ library/src/main/java/com/lazy/library/logging/LogLevel.java
@@ -0,0 +1,16 @@
+package com.lazy.library.logging;
+
+public enum LogLevel {
+    Verbose(Logcat.SHOW_VERBOSE_LOG),
+    Debug(Logcat.SHOW_DEBUG_LOG),
+    Info(Logcat.SHOW_INFO_LOG),
+    Warn(Logcat.SHOW_WARN_LOG),
+    Error(Logcat.SHOW_ERROR_LOG),
+    Json(Logcat.SHOW_JSON_LOG),;
+
+    LogLevel(int value) {
+        this.value = value;
+    }
+
+    final int value;
+}
